c86f579dbc7ab2017f2c7705a1f6e29fddb3e9f8
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-14 17:31:53 +0300

    Cover plain subitems in hyperlink checks
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

---
+++ src/ObjectListView2022.Tests/SubItemTypeSafetyTests.cs
@@ -0,0 +1,20 @@
+using System.Windows.Forms;
+using BrightIdeasSoftware.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ObjectListView2022.Tests
+{
+    [TestClass]
+    public class SubItemTypeSafetyTests
+    {
+        [TestMethod]
+        public void HasAnyHyperlinks_IgnoresPlainListViewSubItems()
+        {
+            var item = new OLVListItem(new object());
+            item.SubItems.Clear();
+            item.SubItems.Add(new ListViewItem.ListViewSubItem(null, "plain"));
+
+            Assert.IsFalse(item.HasAnyHyperlinks);
+        }
+    }
+}
