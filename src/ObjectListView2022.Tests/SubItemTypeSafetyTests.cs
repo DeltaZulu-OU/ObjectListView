168663113be9f4ecae2e0cac80082975a6d5e14f
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-14 20:08:37 +0300

    Cover plain subitems in decoration drawing
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- src/ObjectListView2022.Tests/SubItemTypeSafetyTests.cs
+++ src/ObjectListView2022.Tests/SubItemTypeSafetyTests.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
+using BrightIdeasSoftware;
 using BrightIdeasSoftware.Implementation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ObjectListView2022.Tests
 {
-    [TestClass]
+    [STATestClass]
     public class SubItemTypeSafetyTests
     {
         [TestMethod]
@@ -25,6 +28,27 @@ namespace ObjectListView2022.Tests
             item.SubItems.Add(new ListViewItem.ListViewSubItem(null, "plain"));
 
             Assert.IsNull(item.GetSubItem(0));
+        }
+
+        [TestMethod]
+        public void DrawAllDecorations_IgnoresPlainListViewSubItems()
+        {
+            using var listView = new DecorationProbeObjectListView();
+            var item = new OLVListItem(new object());
+            item.SubItems.Clear();
+            item.SubItems.Add(new ListViewItem.ListViewSubItem(null, "plain"));
+            using var bitmap = new Bitmap(16, 16);
+            using var graphics = Graphics.FromImage(bitmap);
+
+            listView.DrawDecorations(graphics, new List<OLVListItem> { item });
+
+            Assert.IsTrue(true);
+        }
+
+        private sealed class DecorationProbeObjectListView : ObjectListView
+        {
+            public void DrawDecorations(Graphics graphics, List<OLVListItem> items) =>
+                DrawAllDecorations(graphics, items);
         }
     }
 }
