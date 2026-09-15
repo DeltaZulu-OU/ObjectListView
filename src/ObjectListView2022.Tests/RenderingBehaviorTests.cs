2ff1e9f24b258e7d3178129f1434d7d01d6c6672
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-15 11:19:45 +0300

    Cover disabled editing lightbox
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- src/ObjectListView2022.Tests/RenderingBehaviorTests.cs
+++ src/ObjectListView2022.Tests/RenderingBehaviorTests.cs
@@ -7,12 +7,21 @@ namespace ObjectListView2022.Tests
     public class RenderingBehaviorTests
     {
         [TestMethod]
-        public void EditingCellBorderDecoration_ConstructorHonorsLightboxFlag()
+        public void EditingCellBorderDecoration_ConstructorHonorsEnabledLightboxFlag()
         {
             var decoration = new EditingCellBorderDecoration(true);
 
             Assert.IsTrue(decoration.UseLightbox);
             Assert.IsNotNull(decoration.FillBrush);
+        }
+
+        [TestMethod]
+        public void EditingCellBorderDecoration_ConstructorHonorsDisabledLightboxFlag()
+        {
+            var decoration = new EditingCellBorderDecoration(false);
+
+            Assert.IsFalse(decoration.UseLightbox);
+            Assert.IsNull(decoration.FillBrush);
         }
     }
 }
