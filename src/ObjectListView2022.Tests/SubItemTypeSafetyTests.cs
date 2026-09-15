8303991f1d9206af8972303550b4bd44948f169f
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-15 11:15:29 +0300

    Cover valid OLV subitem behavior
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- src/ObjectListView2022.Tests/SubItemTypeSafetyTests.cs
+++ src/ObjectListView2022.Tests/SubItemTypeSafetyTests.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Windows.Forms;
 using BrightIdeasSoftware;
 using BrightIdeasSoftware.Implementation;
+using BrightIdeasSoftware.Rendering;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ObjectListView2022.Tests
@@ -21,6 +22,17 @@ namespace ObjectListView2022.Tests
         }
 
         [TestMethod]
+        public void HasAnyHyperlinks_DetectsOlvSubItemUrl()
+        {
+            var item = new OLVListItem(new object());
+            var subItem = new OLVListSubItem(null, "link", null) { Url = "https://example.invalid/" };
+            item.SubItems.Clear();
+            item.SubItems.Add(subItem);
+
+            Assert.IsTrue(item.HasAnyHyperlinks);
+        }
+
+        [TestMethod]
         public void GetSubItem_ReturnsNullForPlainListViewSubItem()
         {
             var item = new OLVListItem(new object());
@@ -28,6 +40,17 @@ namespace ObjectListView2022.Tests
             item.SubItems.Add(new ListViewItem.ListViewSubItem(null, "plain"));
 
             Assert.IsNull(item.GetSubItem(0));
+        }
+
+        [TestMethod]
+        public void GetSubItem_ReturnsOlvListSubItem()
+        {
+            var item = new OLVListItem(new object());
+            var subItem = new OLVListSubItem(null, "typed", null);
+            item.SubItems.Clear();
+            item.SubItems.Add(subItem);
+
+            Assert.AreSame(subItem, item.GetSubItem(0));
         }
 
         [TestMethod]
@@ -45,10 +68,40 @@ namespace ObjectListView2022.Tests
             Assert.IsTrue(true);
         }
 
+        [TestMethod]
+        public void DrawAllDecorations_DrawsOlvSubItemDecoration()
+        {
+            using var listView = new DecorationProbeObjectListView();
+            var item = new OLVListItem(new object());
+            var subItem = new OLVListSubItem(null, "typed", null);
+            var decoration = new RecordingDecoration();
+            subItem.Decoration = decoration;
+            item.SubItems.Clear();
+            item.SubItems.Add(subItem);
+            using var bitmap = new Bitmap(16, 16);
+            using var graphics = Graphics.FromImage(bitmap);
+
+            listView.DrawDecorations(graphics, new List<OLVListItem> { item });
+
+            Assert.AreEqual(1, decoration.DrawCallCount);
+            Assert.AreSame(item, decoration.ListItem);
+            Assert.AreSame(subItem, decoration.SubItem);
+        }
+
         private sealed class DecorationProbeObjectListView : ObjectListView
         {
             public void DrawDecorations(Graphics graphics, List<OLVListItem> items) =>
                 DrawAllDecorations(graphics, items);
+        }
+
+        private sealed class RecordingDecoration : AbstractDecoration
+        {
+            public int DrawCallCount { get; private set; }
+
+            public override void Draw(ObjectListView olv, Graphics g, Rectangle r)
+            {
+                DrawCallCount++;
+            }
         }
     }
 }
