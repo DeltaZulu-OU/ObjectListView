1b5a7ff5db2790a5b19ed2c88fc0abbd8f33572e
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-16 11:31:05 +0300

    Cover legacy overlay transparency behavior
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- src/ObjectListView2022.Tests/RenderingBehaviorTests.cs
+++ src/ObjectListView2022.Tests/RenderingBehaviorTests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Drawing;
+using System.Reflection;
 using System.Threading;
 using System.Windows.Forms;
 using BrightIdeasSoftware;
@@ -60,6 +62,65 @@ namespace ObjectListView2022.Tests
             thread.Join();
 
             Assert.IsNull(failure, failure?.ToString());
+        }
+
+        [TestMethod]
+        public void ObjectListView_CustomOverlayWithoutTransparency_UsesLegacyOverlayTransparency()
+        {
+            Exception failure = null;
+            double actualOpacity = -1;
+            var thread = new Thread(() =>
+            {
+                try
+                {
+                    using (var form = new Form())
+                    using (var objectListView = new ObjectListView { Dock = DockStyle.Fill })
+                    {
+                        objectListView.OverlayTransparency = 64;
+                        form.Controls.Add(objectListView);
+                        form.Show();
+
+                        objectListView.AddOverlay(new PlainOverlay());
+                        objectListView.ShowOverlays();
+
+                        var glassPanelsField = typeof(ObjectListView).GetField(
+                            "glassPanels",
+                            BindingFlags.Instance | BindingFlags.NonPublic);
+                        var glassPanels = (IEnumerable)glassPanelsField.GetValue(objectListView);
+                        foreach (var glassPanel in glassPanels)
+                        {
+                            actualOpacity = ((Form)glassPanel).Opacity;
+                            break;
+                        }
+
+                        form.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    failure = ex;
+                }
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+
+            Assert.IsNull(failure, failure?.ToString());
+            Assert.AreEqual(64 / 255.0, actualOpacity, 0.0001);
+        }
+
+        [TestMethod]
+        public void ObjectListView_OverlayTransparency_ClampsToByteRange()
+        {
+            using (var objectListView = new ObjectListView())
+            {
+                objectListView.OverlayTransparency = -1;
+                Assert.AreEqual(0, objectListView.OverlayTransparency);
+
+                objectListView.OverlayTransparency = 256;
+                Assert.AreEqual(255, objectListView.OverlayTransparency);
+            }
         }
 
         private sealed class PlainOverlay : IOverlay
